8aedb89b59fc76a9a328875754448d84bca3ee30
J. Rhee <jrhee@fas.harvard.edu>
2013-09-15 02:03:51 -1000

    improved CSS

--- index.php
+++ index.php
@@ -10,58 +10,52 @@
     <!-- CSCI E-15 Project 1 -->
     <!-- - - - - - - - - - - --> 
 <title>CSCI E-15 :: Project 1 :: Jaehyon Rhee (Jay)</title>
-<link rel="stylesheet" type="text/css" href="./css/external.css" />
+  <link rel="stylesheet" type="text/css" href="./css/external.css" />
 </head>
 
 <body>
 
-<table align="center" border="1" cellspacing="0" cellpadding="0" width="1000 pix">
+  <table align="center" border="0" cellspacing="0" cellpadding="0" width="1000 pix">
 
-<tr>
-  <td height="20"></td>
-  <td></td>
-</tr>
+  <tr>
+    <td height="20"></td>
+    <td></td>
+  </tr>
 
-<tr>
-  <td width="160"></td>
-  <td><h2>Jaehyon Rhee (Jay)</h2></td>
-</tr>
+  <tr>
+    <td width="160"></td>
+    <td><h2>Jaehyon Rhee (Jay)</h2></td>
+  </tr>
 
-<tr>
-  <td height="20"></td>
-  <td rowspan="2" align="justify" valign="top">
-  <div style="font-size: 15px;">
-	<p> Hello!  My name is Jay, and I am a professional astronomer.  I am currently working as Assistant Scientist at the 
-	<a href="http://www.gemini.edu" target="_blank">Gemini Observatory</a> in Hilo, HI, 
-	but in late September I will start to work as Instructor at <a href="http://www.oregonstate.edu" target="_blank">
-	Oregon State University</a> in Corvallis, OR.  My initial, primary duty will be to develop three online, 200-level 
-	astronomy courses and to teach them.  I look forward to conducting some research projects as well.
-	I have been living in Hilo by myself due to the job situations of my wife and me, 
-	so I am very glad to be able to be reunited with my family after some 2.5 years!</p>
+  <tr>
+    <td height="18"></td>
+    <td rowspan="2" align="justify" valign="top">
+	<p> Hello!  My name is Jay, and I am a professional astronomer.  I am currently working as Assistant Scientist 
+		at the <a href="http://www.gemini.edu" target="_blank">Gemini Observatory</a> in Hilo, HI, but in late 
+		September I will start to work as Instructor at <a href="http://www.oregonstate.edu" target="_blank">
+		Oregon State University</a> in Corvallis, OR.  My initial, primary duty will be to develop three online, 
+		200-level astronomy courses and to teach them.  I look forward to conducting some research projects as well.
+		I have been living in Hilo by myself due to the job situations of my wife and me, so I am very glad to be 
+		reunited with my family after some 2.5 years!</p>
+	<p> Modern telescopes and instruments produce an enormous amount of high-quality data everyday, and I have 
+		often felt that I need to improve my skills in handling and presenting big data.  As such, I am looking 
+		forward to know more about dynamic, database-driven web applications through this CSCI E-15 course.</p>
+    </td>
+  </tr>
 
-	<p>Modern telescopes and instruments produce an enormous amount of high-quality data everyday, 
-	and I have often felt that I need to improve my skills in handling and presenting big data.
-	As such, I am looking forward to know more about dynamic, database-driven web applications through this CSCI E-15 course.</p>
-
-  </div>
+  <tr>
+  <td style="vertical-align: 0%; align=left">
+      <a href="./images/jrhee_gemini_2013_06_05.jpg" style="background-color:transparent;" target="_blank">
+      <img src="./images/jrhee.jpg" alt="J. Rhee" height="140" width="140" style="padding:0" /></a>
   </td>
-</tr>
+  </tr>
 
-<tr>
-<td style="line-height: 11pt; vertical-align: 0%" width="40" height="140" align="left">
-    <a href="./images/jrhee_gemini_2013_06_05.jpg"
-    style="background-color:transparent;"
-     target="_blank">
-    <img src="./images/jrhee.jpg" alt="J. Rhee" height="140" width="140" style="padding:0" /></a>
-</td>
-</tr>
+  <tr>
+    <td></td>
+    <td><div>The code editor I am using is Sublime Text 2.</div></td>
+  </tr>
 
-<tr>
-  <td></td>
-  <td><div style="font-family: arial; font-style: italic;">The code editor I am using is Sublime Text 2.</div></td>
-</tr>
-
-</table>
+  </table>
 
 </body>
 
