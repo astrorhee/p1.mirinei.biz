b12be47a5eaa9395e8c2c76db58b6c556e1ce89e
J. Rhee <jrhee@fas.harvard.edu>
2013-09-15 00:18:49 -1000

    Added contents to main index page

--- index.php
+++ index.php
@@ -1,11 +1,66 @@
 <!DOCTYPE html>
 
 <html>
-    <body>
 
-        <?php
-        echo "Welcome to P1";
-        ?>
+<head>
+    <meta charset="UTF-8">
+    <!-- - - - - - - - - - - -->
+    <!-- Jaehyon Rhee (Jay)  -->
+    <!-- September 15, 2013  -->
+    <!-- CSCI E-15 Project 1 -->
+    <!-- - - - - - - - - - - --> 
+<title>CSCI E-15 :: Project 1 :: Jaehyon Rhee (Jay)</title>
+<link rel="stylesheet" type="text/css" href="external.css" />
+</head>
 
-    </body>
+<body>
+
+<table align="center" border="0" cellspacing="0" cellpadding="0" width="1000 pix">
+
+<tr>
+  <td height="50"></td>
+  <td></td>
+</tr>
+
+<tr>
+  <td width="150"></td>
+  <td>Jaehyon Rhee (Jay)</td>
+</tr>
+
+<tr>
+  <td height="20"></td>
+  <td rowspan="2" align="justify" valign="top">
+  <div style="font-size: 15px;">
+	<p> Hello!  My name is Jay, and I am a professional astronomer.  I am currently working as Assistant Scientist at the 
+	<a href="http://www.gemini.edu" target="_blank">Gemini Observatory</a> in Hilo, HI, 
+	but in late September I will start to work as Instructor at <a href="http://www.oregonstate.edu" target="_blank">
+	Oregon State University</a> in Corvallis, OR.  My initial, primary duty will be to develop three online, 200-level 
+	astronomy courses and to teach them.  I look forward to conducting some research projects as well.
+	I have been living in Hilo by myself due to the job situations of my wife and me, 
+	so I am very glad to be able to be reunited with my family after some 2.5 years!</p>
+
+	<p>Modern telescopes and instruments produce an enormous amount of high-quality data everyday, 
+	and I have often felt that I need to improve my skills in handling and presenting big data.
+	As such, I am looking forward to know more about dynamic, database-driven web applications through this CSCI E-15 course.</p>
+
+  </div>
+  </td>
+</tr>
+
+<tr>
+<td style="line-height: 11pt; vertical-align: 0%" width="40" height="140" align="left">
+    <a href="./images/jrhee_gemini_2013_06_05.jpg" target="_blank">
+    <img src="./images/jrhee.jpg" alt="J. Rhee" height="140" width="140" style="padding:0" /></a>
+</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td><em>The code editor I am using is Sublime Text 2.</em></td>
+</tr>
+
+</table>
+
+</body>
+
 </html>
