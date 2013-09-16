791c19a2ff65147fdbe2444a2fde6a2c2e0e90c7
J. Rhee <jrhee@fas.harvard.edu>
2013-09-15 18:37:32 -1000

    Modified version to be passed by the W3C Markup Validation check

--- index.php
+++ index.php
@@ -15,63 +15,61 @@
 
 <body>
 
-  <table border="0">
-  <tr>
-    <td style="height:25px;"></td>
-  </tr>
+  <table>
+    <tr>
+      <td style="height: 25px;"></td>
+    </tr>
   </table>
 
-<table border="0" width="940" align="center">
-  <tr>
-    <td valign="top" align="center">
+  <table style="width: 860px; margin: auto;">
+    <tr>
+      <td>
 
+        <div class='window-outer'>
+          <table style="width: 820px; margin: auto;">
 
-<div class='window-outer'>
-  <table align="center" border="0" width="900 pix">
+            <tr>
+              <td style="width: 150px;"></td>
+              <td><h2>Jaehyon Rhee (Jay)</h2></td>
+            </tr>
 
-  <tr>
-    <td style="width:150px;"></td>
-    <td><h2>Jaehyon Rhee (Jay)</h2></td>
-  </tr>
+            <tr>
+              <td style="height: 5px;"></td>
+              <td rowspan="2" style="text-align: justify; vertical-align: top;">
+	              <p> Hello!  My name is Jay, and I am a professional astronomer.  I am currently working as Assistant Scientist 
+		                at the <a href="http://www.gemini.edu" target="_blank">Gemini Observatory</a> in Hilo, HI, but in late 
+		                September I will start to work as Instructor at <a href="http://www.oregonstate.edu" target="_blank">
+		                Oregon State University</a> in Corvallis, OR.  My initial, primary duty will be to develop three online, 
+		                200-level astronomy courses and to teach them.  I look forward to conducting some research projects as well.
+		                I have been living in Hilo by myself due to the job situations of my wife and me, so I am very glad to be 
+		                reunited with my family after some 2.5 years!</p>
+	              <p> Modern telescopes and instruments produce an enormous amount of high-quality data everyday, and I have 
+		                often felt that I need to improve my skills in handling and presenting big data.  As such, I am looking 
+		                forward to know more about dynamic, database-driven web applications through this 
+                    <a href="http://dwa15.com" target="_blank">CSCI E-15</a> course.</p>
+              </td>
+            </tr>
 
-  <tr>
-    <td style="height:5px;"></td>
-    <td rowspan="2" align="justify" valign="top">
-	<p> Hello!  My name is Jay, and I am a professional astronomer.  I am currently working as Assistant Scientist 
-		at the <a href="http://www.gemini.edu" target="_blank">Gemini Observatory</a> in Hilo, HI, but in late 
-		September I will start to work as Instructor at <a href="http://www.oregonstate.edu" target="_blank">
-		Oregon State University</a> in Corvallis, OR.  My initial, primary duty will be to develop three online, 
-		200-level astronomy courses and to teach them.  I look forward to conducting some research projects as well.
-		I have been living in Hilo by myself due to the job situations of my wife and me, so I am very glad to be 
-		reunited with my family after some 2.5 years!</p>
-	<p> Modern telescopes and instruments produce an enormous amount of high-quality data everyday, and I have 
-		often felt that I need to improve my skills in handling and presenting big data.  As such, I am looking 
-		forward to know more about dynamic, database-driven web applications through this CSCI E-15 course.</p>
-    </td>
-  </tr>
+            <tr>
+              <td style="vertical-align: top;">
+                <a href="./images/jrhee_gemini_2013_06_05.jpg" style="background-color:transparent;" target="_blank">
+                  <img src="./images/jrhee.jpg" alt="J. Rhee" height="140" width="140" style="padding:0;" /></a>
+              </td>
+            </tr>
 
-  <tr>
-  <td style="vertical-align: 0%; align=left">
-      <a href="./images/jrhee_gemini_2013_06_05.jpg" style="background-color:transparent;" target="_blank">
-      <img src="./images/jrhee.jpg" alt="J. Rhee" height="140" width="140" style="padding:0" /></a>
-  </td>
-  </tr>
+          </table>
+        </div>
 
+          <table style="width: 820px; margin: auto;">
+            <tr>
+              <td style="width: 155px;"></td>
+              <td><p style="font-style: italic;">* The code editor I used to make this webpage was Sublime Text 2.</p></td>
+            </tr>
+          </table>
+
+      </td>
+    </tr>
   </table>
-</div>
-
-  <table align="center" border="0" width="900 pix">
-  <tr>
-    <td style="width:155px;"></td>
-    <td><p style="font-style: italic;">* The code editor I used to make this webpage was Sublime Text 2.</p></td>
-  </tr>
-  </table>
-
-
-</td>
-</tr>
-</table>
-
 
 </body>
 
